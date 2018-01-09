fc846e252936f89babf940fd88bc9f6a96badb3a
gres <mggicay@gmail.com>
2018-01-09 11:08:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-exports.helloHttp = function helloHttp (req, res){
-	response = "This is a sample response from your webhook!"
+'use strict';
 
-	res.setHeader('Content-Type', 'application/json');
-	res.send(JSON.stringify*({"speech":response, "displayText":response}));
+const http = require('http');
+const host = 'https://make-sandwich.herokuapp.com/';
+const wwoApiKey = '6f5c9852b94a4690bf562fe99dd7f9cb';
+exports.sandwichWebhook = (req, res)=>{
+	let meat = req.body.result.parameters['meat'];
+	let condimetns = req.body.result.parameters['condiments'];
+	callSandwichAPI(meat, condiments).then((output)=>{
+		res.setHeader('Content-Type','application/json');
+		res.send(JSON.stringify({'speech':output,'displayText':output}));
+	}).catch((error)=>{
+		res.setHeader('Content-Type','application/json');
+		res.send(JSON.stringify({'speech':error, 'displayText':error}));
+	});
+};
+
+function callSandwichAPI (meat, condiments){
+	return new Promise ((resolve, reject)=>{
+		console.log('API Request: ' + host);
+	});
 }
