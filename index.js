4d380728373923606c914aed33d1d236e66a594c
gres <mggicay@gmail.com>
2018-01-10 17:44:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var express = require('express')
 var app = express()
 
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
