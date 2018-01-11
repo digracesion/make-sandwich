9119d54b97349a5059cfc4e0660804637a379459
gres <mggicay@gmail.com>
2018-01-11 12:00:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var app = express();
-var path = require('path');
-var PORT = process.env.PORT || 5000
-var http = require('http');
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  // .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// var path = require('path');
+// var PORT = process.env.PORT || 5000
 
-setInterval(function(){
-	http.get("make-sandwich.herokuapp.com");
-}, 300000); //awale every 5 minutes
+app.set('port', (process.env.PORT || 5000));
 
-console.log("sample");
+app.use(express.static(__dirname + '/public'));
+
+//set up views
+app.set('views',__dirname);
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response){
+	response.render('index.js');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Node app is running on port ', app.get('port'));
+});
