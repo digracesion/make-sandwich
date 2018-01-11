7dd10b73afc4f55895a1a89a3419ca81d70b2824
gres <mggicay@gmail.com>
2018-01-11 09:24:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-var express = require('express')
-var app = express()
-
+var express = require('express');
+var app = express();
+var path = require('path');
+var PORT = process.env.PORT || 5000
+var http = require('http');
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
+  // .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-var http = require('http');
 setInterval(function(){
 	http.get("make-sandwich.herokuapp.com");
 }, 300000); //awale every 5 minutes
+
+console.log("sample");
